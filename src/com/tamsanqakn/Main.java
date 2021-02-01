f11987853d439237fc2dd4a69f097b1818ab4df1
TamKeith <tamkeithncube@gmail.com>
2021-02-01 13:23:59 +0200

    Initial Setup of the ktoc

--- src/com/tamsanqakn/Main.java
+++ src/com/tamsanqakn/Main.java
@@ -1,8 +1,20 @@
 package com.tamsanqakn;
 
+import java.util.Scanner;
+
 public class Main {
 
+    static double kelvin = 0;
+    static double celcius = 0;
+
     public static void main(String[] args) {
-	// write your code here
+
+        boolean quit = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the ****Kelvin to Celcius Converter****");
+
+
+        scanner.close();
+        System.out.println("Thank You for using the ****Kelvin to Celcius Converter****. Bye");
     }
 }
